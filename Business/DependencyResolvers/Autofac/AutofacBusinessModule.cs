96f05c2bd4a3c3cca86c6fc412e6656c69adc0af
Zeynel OK <zeynel_2406@hormail.com>
2021-02-26 22:11:09 +0300

    Fluent validation added with AOP

--- Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -1,11 +1,11 @@
 ﻿using Autofac;
+using Autofac.Extras.DynamicProxy;
 using Business.Abstract;
 using Business.Concrete;
+using Castle.DynamicProxy;
+using Core.Utilities.Interceptors;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Business.DependencyResolvers.Autofac
 {
@@ -30,6 +30,12 @@ namespace Business.DependencyResolvers.Autofac
             builder.RegisterType<EfUserDal>().As<IUserDal>().SingleInstance();
 
 
+            var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+            builder.RegisterAssemblyTypes(assembly).AsImplementedInterfaces()
+                .EnableInterfaceInterceptors(new ProxyGenerationOptions()
+                {
+                    Selector = new AspectInterceptorSelector()
+                }).SingleInstance();
         }
     }
 }
---
+++ Business/ValidationRules/FluentValidation/CarValidator.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+   public class CarValidator:AbstractValidator<Car>
+    {
+        public CarValidator()
+        {
+            RuleFor(x => x.CarName).NotEmpty();
+            RuleFor(x => x.CarName).MinimumLength(2);
+            RuleFor(x => x.DailyPrice).GreaterThan(0);
+        }
+    }
+}
